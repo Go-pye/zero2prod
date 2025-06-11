8ce8e61b89aec56078a4d20846f69be860ad00f3
stacks-of-cache <fattyeleven@proton.me>
2025-06-10 18:27:01 -0700

    test: add test for 500 errors

--- src/email_client.rs
+++ src/email_client.rs
@@ -58,7 +58,8 @@ impl EmailClient {
       )
       .json(&request_body)
       .send()
-      .await?;
+      .await?
+      .error_for_status()?;
 
     Ok(())
   }
@@ -75,7 +76,7 @@ mod tests {
   use wiremock::matchers::{header, header_exists, path, method};
   use wiremock::matchers::any;
   use wiremock::Request;
-  use claims::assert_ok;
+  use claims::{assert_ok, assert_err};
   use super::*;
 
   struct SendEmailBodyMatcher;
@@ -139,4 +140,25 @@ mod tests {
 
     assert_ok!(outcome);
   }
+
+  #[tokio::test]
+  async fn send_email_fails_if_the_server_returns_500() {
+    let mock_server = MockServer::start().await;
+    let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+    let email_client = EmailClient::new(mock_server.uri(), sender, Secret::new(Faker.fake()));
+
+    let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+    let subject: String = Sentence(1..2).fake();
+    let content: String = Paragraph(1..10).fake();
+
+    Mock::given(any()).respond_with(ResponseTemplate::new(500))
+      .expect(1)
+      .mount(&mock_server)
+      .await;
+
+    let outcome = email_client.send_email(subscriber_email, &subject, &content, &content).await;
+
+    assert_err!(outcome);
+  }
+
 }
